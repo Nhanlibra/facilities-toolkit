288811f6ab660ebad197077b3a25ea3c96853985
Nathan Opresnik <nopresnik@me.com>
2021-04-19 19:39:30 +1000

    Style nav links

--- client/src/components/sidebar/nav/NavItem.js
+++ client/src/components/sidebar/nav/NavItem.js
@@ -1,7 +1,40 @@
+import React, { useState } from "react";
+
 const NavItem = (props) => {
+  const liClassNames = (hover) => {
+    if (hover) {
+      return `
+        bg-gray-200
+        rounded-md text-gray-800 py-1 px-2
+        flex items-center cursor-pointer
+      `;
+    }
+
+    return `
+      ${props.active ? "bg-gray-200" : ""}
+      rounded-md text-gray-800 py-1 px-2
+      flex items-center cursor-pointer
+    `;
+  };
+
+  const [hover, setHover] = useState(false);
+
+  const handleHover = (e) => {
+    e.target.className = liClassNames(!hover);
+    setHover(!hover);
+  };
+
   return (
-    <li>
-      <i className={`${props.icon} mr-3`}></i>
+    <li
+      className={`
+        ${props.active ? "bg-gray-200" : ""}
+        rounded-md text-gray-800 py-1 px-2
+        flex items-center
+      `}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+    >
+      <i className={`${props.icon} mr-3 text-2xl text-gray-600`}></i>
       {props.text}
     </li>
   );
--- client/src/components/sidebar/nav/index.js
+++ client/src/components/sidebar/nav/index.js
@@ -1,15 +1,17 @@
 import NavItem from "./NavItem";
 
 const links = [
-  { name: "Home", icon: "bi-house-door" },
+  { name: "Home", icon: "bi-house-door", active: true },
   { name: "Dashboard", icon: "bi-speedometer" },
 ];
 
 const Nav = () => {
   return (
-    <ul className="space-y-2">
+    <ul className="space-y-1">
       {links.map((link) => {
-        return <NavItem text={link.name} icon={link.icon} />;
+        return (
+          <NavItem text={link.name} icon={link.icon} active={link.active} />
+        );
       })}
     </ul>
   );
