6f2b3075f4d9b37018516e76b23fb969039b711d
Nathan Opresnik <nopresnik@me.com>
2021-04-29 20:08:45 +1000

    Fix styling and render call type buttons

--- client/src/pages/NewCall/index.js
+++ client/src/pages/NewCall/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Modal, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Modal, Row } from "react-bootstrap";
 
 export default ({ show, handleClose }) => {
   return (
@@ -9,15 +9,21 @@ export default ({ show, handleClose }) => {
       backdrop="static"
       keyboard={false}
       centered
-      // size="xl"
+      size="lg"
     >
       <Modal.Header closeButton>
         <Modal.Title>Log New Call</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Row noGutters>
-          <Col sm={4}>
+          <Col sm={3}>
             <NumPad />
+          </Col>
+          <Col sm={3}>
+            <CallForm />
+          </Col>
+          <Col sm={6}>
+            <CallTypeList />
           </Col>
         </Row>
       </Modal.Body>
@@ -35,12 +41,13 @@ export default ({ show, handleClose }) => {
 
 const NumPad = () => {
   const NumBtn = ({ value }) => (
-    <Col xs={value === 0 ? 8 : 4}>
-      <Button variant="secondary" style={{ border: "1px solid black" }} block>
+    <Col style={{ padding: "1px" }} xs={value === 0 ? 8 : 4}>
+      <Button variant="secondary" size="lg" block>
         {value}
       </Button>
     </Col>
   );
+
   return (
     <Container>
       <Row noGutters>
@@ -55,6 +62,79 @@ const NumPad = () => {
         <NumBtn value={3} />
         <NumBtn value={0} />
         <NumBtn value="C" />
+      </Row>
+    </Container>
+  );
+};
+
+const CallForm = () => {
+  return (
+    <Form>
+      <Form.Group controlId="formLane">
+        <Form.Label>Lane</Form.Label>
+        <Form.Control type="number" />
+      </Form.Group>
+      <Form.Group controlId="formLane">
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" />
+      </Form.Group>
+    </Form>
+  );
+};
+
+const CallTypeList = () => {
+  const codes = [
+    "Pin OOR",
+    "Pin 1 Ld",
+    "Pin 2 Ld",
+    "Pin 3 Ld",
+    "Pin 4 Ld",
+    "Pin 5 Ld",
+    "Pin 6 Ld",
+    "Pin 7 Ld",
+    "Pin 8 Ld",
+    "Pin 9 Ld",
+    "Pin 10 Ld",
+    "A Found",
+    "B Found",
+    "C Found",
+    "D Found",
+    "SM Found",
+    "G Found",
+    "STFound",
+    "OORFound",
+    "A NtFnd",
+    "B NtFnd",
+    "C NtFnd",
+    "D NtFnd",
+    "SM NtFnd",
+    "G NtFnd",
+    "STNtFnd",
+    "Invld 0",
+    "Invld 1",
+    "Invld 2",
+    "Invld 3",
+    "Invld 4",
+    "Invld 5",
+    "ElevJam",
+    "Pin Cnt",
+    "TS1 Jam",
+    "TS2 Jam",
+    "AcelOff",
+    "IL",
+    "PwrFail",
+  ];
+
+  return (
+    <Container>
+      <Row noGutters>
+        {codes.map((code) => (
+          <Col xs={4} style={{ padding: "2px" }}>
+            <Button size="sm" block>
+              {code}
+            </Button>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
