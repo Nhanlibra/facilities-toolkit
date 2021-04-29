131f330a9e961517e61c2735aa62e9d03802b1db
Nathan Opresnik <nopresnik@me.com>
2021-04-29 23:37:58 +1000

    Add custom colours for priority badges

--- client/src/Pages/RepairsRequired/index.js
+++ client/src/Pages/RepairsRequired/index.js
@@ -21,7 +21,7 @@ export default () => {
             <td>Thu 29 Apr 2021 20:48</td>
             <td>5</td>
             <td>
-              <Badge variant="danger">High</Badge>
+              <Badge variant="priority-high">High</Badge>
             </td>
             <td>Repair/replace 7 pin holder to solve double loading.</td>
             <td>
@@ -33,7 +33,7 @@ export default () => {
             <td>Thu 29 Apr 2021 20:23</td>
             <td>23</td>
             <td>
-              <Badge variant="info">Low</Badge>
+              <Badge variant="priority-low">Low</Badge>
             </td>
             <td>Cracked 7 pin side T-Band frame</td>
             <td>
@@ -63,7 +63,7 @@ export default () => {
             <td>Fri 2 Apr 2021 10:02</td>
             <td>12</td>
             <td>
-              <Badge variant="warning">Medium</Badge>
+              <Badge variant="priority-medium">Medium</Badge>
             </td>
             <td>Repair/replace #1 spotting tong</td>
             <td>
