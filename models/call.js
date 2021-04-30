c37f5a6cd71346043d8f58272111ac2e81100db7
Nathan Opresnik <nopresnik@me.com>
2021-04-30 20:27:53 +1000

    Create call model

---
+++ models/call.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoos");
+
+const Schema = mongoose.Schema;
+
+const callSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  lane: {
+    type: Number,
+    required: "Call lane number is required",
+  },
+  code: {
+    type: String,
+    required: "Call code is required",
+  },
+  description: String,
+  tech: {
+    type: String,
+    required: "Call tech is required",
+  },
+});
+
+const Call = mongoose.model("Calls", callSchema);
+module.exports = Call;
