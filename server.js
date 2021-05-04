0e529fd7cb18466ea15eebdec6c2bb9fbe2726f4
Nathan Opresnik <nopresnik@me.com>
2021-05-04 21:23:02 +1000

    Fix deprecation warning

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ mongoose.connect(
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     },
     () => console.log('Connected to MongoDB'),
 );
