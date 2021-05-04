950c3ff061538282de42af1ab9dae00e5d3b29b5
Nathan Opresnik <nopresnik@me.com>
2021-05-04 22:51:13 +1000

    Add auth consumer to app

--- client/src/App.js
+++ client/src/App.js
@@ -6,41 +6,44 @@ import CallHistory from './Pages/CallHistory';
 import Dashboard from './Pages/Dashboard';
 import PendingWork from './Pages/PendingWork';
 import RepairsRequired from './Pages/RepairsRequired';
+import {ProvideAuth} from './hooks/useAuth';
 
 function App() {
   return (
-    <Router>
-      <Container fluid>
-        <Row>
-          <Col
-            xs={2}
-            md={3}
-            lg={3}
-            xl={2}
-            className="position-sticky vh-100"
-            style={{top: 0}}
-          >
-            <Sidebar />
-          </Col>
-          <Col xs={10} md={9} lg={9} xl={10} className="py-3">
-            <Switch>
-              <Route path="/calls">
-                <CallHistory />
-              </Route>
-              <Route path="/repairs">
-                <RepairsRequired />
-              </Route>
-              <Route path="/pendingwork">
-                <PendingWork />
-              </Route>
-              <Route path="/">
-                <Dashboard />
-              </Route>
-            </Switch>
-          </Col>
-        </Row>
-      </Container>
-    </Router>
+    <ProvideAuth>
+      <Router>
+        <Container fluid>
+          <Row>
+            <Col
+              xs={2}
+              md={3}
+              lg={3}
+              xl={2}
+              className="position-sticky vh-100"
+              style={{top: 0}}
+            >
+              <Sidebar />
+            </Col>
+            <Col xs={10} md={9} lg={9} xl={10} className="py-3">
+              <Switch>
+                <Route path="/calls">
+                  <CallHistory />
+                </Route>
+                <Route path="/repairs">
+                  <RepairsRequired />
+                </Route>
+                <Route path="/pendingwork">
+                  <PendingWork />
+                </Route>
+                <Route path="/">
+                  <Dashboard />
+                </Route>
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
+    </ProvideAuth>
   );
 }
 
