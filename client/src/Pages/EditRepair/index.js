afc3a16c07a81a0b17daf98870056bc8fe9fe2ba
Nathan Opresnik <nopresnik@me.com>
2021-05-10 00:45:58 +1000

    Handle editing repairs

--- client/src/Pages/EditRepair/index.js
+++ client/src/Pages/EditRepair/index.js
@@ -1,42 +1,79 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Modal} from 'react-bootstrap';
+import RepairForm from '../../components/RepairForm';
+import {useAuth} from '../../hooks/useAuth';
 
 const EditRepair = ({show, handleClose, data}) => {
+  const {user} = useAuth();
+  const [repairData, setRepairData] = useState();
+  const [changesMade, setChangesMade] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      setRepairData(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (repairData && changesMade) {
+      if (repairData.status === 'completed') {
+        // console.log(repairData);
+        handleSubmit();
+      }
+    }
+  }, [repairData]);
+
   const handleSubmit = () => {
-    console.log('Not yet implemented');
+    // console.log(JSON.stringify(repairData, null, 2));
+    handleClose();
+  };
+
+  const handleComplete = () => {
+    setRepairData({
+      ...repairData,
+      status: 'completed',
+      repairedBy: user.initials,
+      dateRepaired: new Date(),
+    });
+    setChangesMade(true);
   };
 
   return (
     <>
-      {data &&
-      <Modal
-        show={show}
-        onHide={handleClose}
-        keyboard={false}
-        centeredsize="md"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Log New Repair</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-        Cancel
-          </Button>
-          <Button
-            variant="primary"
-            onClick={handleSubmit}
-          >
-        Log repairs
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      {repairData &&
+        <Modal
+          show={show}
+          onHide={handleClose}
+          keyboard={false}
+          centered
+          size="md"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Repair</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <RepairForm state={repairData} setState={setRepairData} />
+          </Modal.Body>
+          <Modal.Footer>
+            {repairData.status !== 'completed' &&
+              <Button variant="success" onClick={handleComplete}>
+                Complete
+              </Button>
+            }
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button
+              variant="primary"
+              onClick={handleSubmit}
+            >
+              Save repair
+            </Button>
+          </Modal.Footer>
+        </Modal>
       }
     </>
   );
 };
-
 
 export default EditRepair;
