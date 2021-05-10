2281513c25580f9ed8658040987250f243ae9821
Nathan Opresnik <nopresnik@me.com>
2021-05-10 16:17:19 +1000

    Style login screen

--- client/src/Pages/Login/index.js
+++ client/src/Pages/Login/index.js
@@ -1,5 +1,7 @@
-import {ErrorMessage, Field, Form, Formik} from 'formik';
+import {ErrorMessage, Field, Formik, Form} from 'formik';
 import React from 'react';
+import {Col, Container, Row} from 'react-bootstrap';
+import {Form as RBForm} from 'react-bootstrap';
 import {Redirect} from 'react-router-dom';
 import {useAuth} from '../../hooks/useAuth';
 
@@ -12,37 +14,63 @@ const Login = () => {
   }
 
   return (
-    <>
-      <h1>Login page</h1>
-      <Formik
-        initialValues={{email: '', password: ''}}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, {setSubmitting}) => {
-          login(values);
-          setSubmitting(false);
-        }}>
-        {({isSubmitting}) => (
-          <Form>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <Field type="password" name="password" />
-            <button name="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </>
+    <Container fluid>
+      <Row>
+        <Col md={5} className="vh-100 login-cover" />
+        <Col md={7}
+          className="vh-100 d-flex justify-content-center align-items-center"
+        >
+          <div className="w-50">
+            <h2>Welcome Back!</h2>
+            <Formik
+              initialValues={{email: '', password: ''}}
+              validate={(values) => {
+                const errors = {};
+                if (!values.email) {
+                  errors.email = 'Required';
+                } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                  errors.email = 'Invalid email address';
+                }
+                return errors;
+              }}
+              onSubmit={(values, {setSubmitting}) => {
+                login(values);
+                setSubmitting(false);
+              }}>
+              {({isSubmitting}) => (
+                <Form component={RBForm}>
+                  <RBForm.Group>
+                    <RBForm.Label>Email address</RBForm.Label>
+                    <Field
+                      type="email"
+                      name="email"
+                      className="form-control"
+                    />
+                    <ErrorMessage name="email" component="div" />
+                  </RBForm.Group>
+                  <RBForm.Group>
+                    <RBForm.Label>Password</RBForm.Label>
+                    <Field
+                      type="password"
+                      name="password"
+                      className="form-control"
+                    />
+                  </RBForm.Group>
+                  <button className="btn btn-primary"
+                    name="submit"
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
